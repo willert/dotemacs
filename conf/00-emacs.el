35a0223df0eff51065e55a1357e3b19ae456219c
Sebastian Willert <willert@gmail.com>
2011-03-29 13:55:34 +0200

    added comment about warnings

--- conf/00-emacs.el
+++ conf/00-emacs.el
@@ -2,6 +2,9 @@
 ;; Emacs behavior
 ;; ---------------------------------------------------------------------------
 
+; Warning behavior:
+; http://www.gnu.org/s/emacs/manual/html_node/elisp/Warning-Options.html
+
 (setq inhibit-splash-screen t)
 (if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
