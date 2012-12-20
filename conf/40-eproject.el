719d1ce0299c9c0a58af581ddbda2ed1fd973ab3
Sebastian Willert <willert@gmail.com>
2012-12-20 03:18:08 +0100

    Many improvements to project shells

--- conf/40-eproject.el
+++ conf/40-eproject.el
@@ -83,19 +83,30 @@
   )
 
 (defun sbw/perl-project-execute-local-init ()
-  (message "Root: %s" (eproject-root))
+  ; (message "Root: %s" (eproject-root))
   (let ((init-file
          (replace-regexp-in-string
           "//+" "/"
           (concat (eproject-root) "/.emacs.d/init.el"))))
-    (if (file-exists-p init-file) (load init-file)))
-  )
+    (if (file-exists-p init-file) (load init-file))))
+
+
+(defun sbw/comint-send-input-at-eob ()
+  "Send input to shell if cursor is after the prompt,
+else advance a line"
+  (interactive)
+  (if (comint-after-pmark-p)
+      (call-interactively 'comint-send-input)
+    (call-interactively 'next-line)))
+
 
 (defun sbw/open-shell-in-project-root ()
+  "Open a shell in project root directory"
   (interactive)
   (let* ((project-root (eproject-root))
-         (default-directory project-root)
+         (project-type (eproject-type))
          (project-name (car (last (split-string project-root "/" t))))
+         (default-directory project-root)
          (shell-postfix
           (if current-prefix-arg
               (read-string "Buffer type (defaults to 'shell'): " nil
@@ -103,19 +114,51 @@
             "shell"))
          (buffer-name (concat project-name "-" shell-postfix ))
          (buffer (switch-to-buffer buffer-name))
-        )
-    (with-current-buffer buffer
-      (shell buffer)
+         )
+    (if (get-buffer-process (current-buffer))
+        (message "Switching to pre-existing shell")
+      (with-current-buffer buffer
+        (shell buffer)
+        (make-variable-buffer-local 'comint-prompt-read-only)
+        (setq comint-prompt-read-only t)
+        (local-set-key (kbd "<return>") 'sbw/comint-send-input-at-eob)
 
-      (let ((process (get-buffer-process (current-buffer)))
-            )
-        (unless process
-          (error "No process in %s" buffer-or-name))
-        (goto-char (process-mark process))
-        (insert "cd " project-root "; source perl5/etc/mist.mistrc")
-        (comint-send-input nil t)
-        ))
-    ))
+        (let ((process (get-buffer-process (current-buffer))))
+          (unless process
+            (error "No process in %s" buffer-or-name))
+
+          (set-process-query-on-exit-flag process nil)
+          (set-process-sentinel
+           process
+           (lambda (this-process state)
+             (if (or (string-match "exited abnormally with code.*" state)
+                     (string-match "finished" state))
+                 (kill-buffer (current-buffer)))
+             ))
+
+          (goto-char (process-mark process))
+          (insert
+           "cd " project-root ";"
+           "source perl5/etc/mist.mistrc 2> /dev/null" ";"
+           "export HISTFILE=\"" project-root "/.bash_history_local\"" ";"
+           "history -r" ";"
+           )
+          (let ((comint-process-echoes t))
+            (comint-send-input nil t)
+            (sbw/clear-shell))
+          )
+
+        (progn ; the guts of (eproject-maybe-turn-on)
+          (make-variable-buffer-local 'eproject-root)
+          (setq eproject-root project-root)
+          (eproject--init-attributes project-root project-type)
+          (eproject-mode 1)
+
+                                        ; eproject--setup-local-variables works on file and dired buffers
+                                        ; so lets pretend we are in dired mode instead of shell mode
+          (let ((major-mode 'dired-mode))
+            (eproject--setup-local-variables)))
+        ))))
 
 (add-hook 'perl-project-file-visit-hook 'sbw/perl-project-compile-command)
 (add-hook 'perl-project-file-visit-hook 'sbw/perl-project-ack-arguments)
