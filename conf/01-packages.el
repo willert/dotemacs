e52cd48862765e3b27e1c344ce2d7b8162f3e30e
Sebastian Willert <willert@gmail.com>
2012-12-20 03:19:53 +0100

    Added new package source

--- conf/01-packages.el
+++ conf/01-packages.el
@@ -3,6 +3,7 @@
       '(("ELPA" . "http://tromey.com/elpa/")
         ("gnu" . "http://elpa.gnu.org/packages/")
         ("sunrise-commander" . "http://joseito.republika.pl/sunrise-commander/")
+        ("melpa" . "http://melpa.milkbox.net/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/"))
 )
 (package-initialize)
