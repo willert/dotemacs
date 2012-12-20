1cc3b99c0b5522468632206c75a1ee4d379dff18
Sebastian Willert <willert@gmail.com>
2012-12-20 03:21:12 +0100

    Moved dired config to own file

---
+++ conf/30-dired.el
@@ -0,0 +1,30 @@
+;; allow dired to be able to delete or copy a whole dir.
+;; “always” means no asking. “top” means ask once. Any other symbol means ask each and every time for a dir and subdir.
+(setq dired-recursive-copies (quote always))
+(setq dired-recursive-deletes (quote top))
+
+;; use other as target if two dired-buffers are visible
+(setq dired-dwim-target t)
+
+; dired-single
+(require 'dired-single)
+(defun sbw/dired-init ()
+  "Bunch of stuff to run for dired, either immediately or when it's
+   loaded."
+  ;; <add other stuff here>
+  (define-key dired-mode-map [return] 'dired-single-buffer)
+  (define-key dired-mode-map [mouse-1] 'dired-single-buffer-mouse)
+  (define-key dired-mode-map "^"
+    (function
+     (lambda nil (interactive) (dired-single-buffer ".."))))
+  (define-key dired-mode-map [backspace]
+    (function
+     (lambda nil (interactive) (dired-single-buffer ".."))))
+
+)
+;; if dired's already loaded, then the keymap will be bound
+(if (boundp 'dired-mode-map)
+  ;; we're good to go; just add our bindings
+  (sbw/dired-init)
+  ;; it's not loaded yet, so add our bindings to the load-hook
+  (add-hook 'dired-load-hook 'sbw/dired-init))
