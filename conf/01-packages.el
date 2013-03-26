96193d475816d2ddebb6412f09f274119e0cff41
Sebastian Willert <willert@gmail.com>
2013-03-26 02:48:09 +0100

    Removed ELPA source for aunrise commander

--- conf/01-packages.el
+++ conf/01-packages.el
@@ -2,7 +2,6 @@
 (setq package-archives
       '(("ELPA" . "http://tromey.com/elpa/")
         ("gnu" . "http://elpa.gnu.org/packages/")
-        ("sunrise-commander" . "http://joseito.republika.pl/sunrise-commander/")
         ("melpa" . "http://melpa.milkbox.net/packages/")
         ("marmalade" . "http://marmalade-repo.org/packages/"))
 )
