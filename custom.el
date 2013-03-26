4b3bda7223d32c242e3ba6ac9ff39e4c0002f487
Sebastian Willert <willert@gmail.com>
2013-03-26 02:48:17 +0100

    Font changes

--- custom.el
+++ custom.el
@@ -10,4 +10,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(whitespace-tab ((t (:foreground "#123838"))))
+ '(whitespace-trailing ((t (:background "#093030")))))
