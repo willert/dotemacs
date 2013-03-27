1b475e9f4a711d395891b6f5c3bb956316bbd02a
Sebastian Willert <willert@gmail.com>
2013-03-27 10:15:10 +0100

    Fixed magit installation

--- custom.el
+++ custom.el
@@ -10,5 +10,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(diff-file-header ((t (:background "dark slate gray" :weight bold))))
+ '(highlight ((t (:background "midnight blue"))))
+ '(magit-diff-file-header ((t (:inherit magit-diff-hunk-header :box (:line-width 2 :color "#0b2626")))))
+ '(magit-diff-hunk-header ((t (:slant italic :weight bold))))
+ '(magit-item-highlight ((t (:background "dark slate gray"))))
  '(whitespace-tab ((t (:foreground "#123838"))))
  '(whitespace-trailing ((t (:background "#093030")))))
