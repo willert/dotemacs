463e3c33bfe3855ee099a601f29a6505c173296f
Sebastian Willert <willert@gmail.com>
2013-10-10 20:08:27 +0200

    Allow a few custom compass tasks

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(csv-separators (quote (";" "~")))
- '(safe-local-variable-values (quote ((sbw/prove-project-directories . "t/ t/acceptance/")))))
+ '(safe-local-variable-values (quote ((sbw/prove-project-directories . "t/ t/acceptance/") (compass-command . "grunt") (compass-task . "default")))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
