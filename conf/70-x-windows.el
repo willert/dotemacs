85a59e3d45eca6fa5a5e6689b0a771dcd933c6e2
Sebastian Willert <willert@gmail.com>
2014-03-19 08:21:19 +0100

    Move session files to their own dir

--- conf/70-x-windows.el
+++ conf/70-x-windows.el
@@ -9,4 +9,17 @@
   (add-hook 'after-make-frame-functions 'sbw/adjust-terminal-colors t)
   (add-hook 'after-make-frame-functions 'sbw/color-theme-nighttimes)
 
-  (add-hook 'after-make-frame-functions 'sbw/run-devilspie t)))
+  (add-hook 'after-make-frame-functions 'sbw/run-devilspie t)
+
+  (require 'x-win)
+
+  (defun emacs-session-filename (session-id)
+    "Construct a filename to save the session in based on SESSION-ID
+     in  ~/.emacs.d/var"
+  (let ((basename (concat "session." session-id))
+        (emacs-dir user-emacs-directory))
+    (make-directory (expand-file-name (concat emacs-dir "var")))
+    (expand-file-name (concat emacs-dir "var/" basename))
+    ))
+
+))
