505a4f5153ddaf7fbb1007c35411dffac1be5ad0
Sebastian Willert <willert@gmail.com>
2014-10-07 03:48:05 +0200

    Added twittering mode

---
+++ elpa/twittering-mode-20140420.1005/twittering-mode-autoloads.el
@@ -0,0 +1,29 @@
+;;; twittering-mode-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads (twit) "twittering-mode" "twittering-mode.el" (21549
+;;;;;;  20566 727961 562000))
+;;; Generated autoloads from twittering-mode.el
+
+(autoload 'twit "twittering-mode" "\
+Start twittering-mode.
+
+\(fn)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("twittering-mode-pkg.el") (21549 20566
+;;;;;;  745195 750000))
+
+;;;***
+
+(provide 'twittering-mode-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; twittering-mode-autoloads.el ends here
