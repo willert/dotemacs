f186aae26e28a9367b5892d6c7feb8f49584180e
Sebastian Willert <willert@gmail.com>
2018-08-01 03:20:40 +0200

    Deleted lots of unused packages

--- elpa/moz-20090407.1405/moz-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "moz" "20090407.1405" "Lets current buffer interact with inferior mozilla." 'nil :url "http://github.com/bard/mozrepl/raw/master/chrome/content/moz.el")
--- elpa/moz-controller-20141001.2347/moz-controller-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "moz-controller" "20141001.2347" "Control Firefox from Emacs" '((moz "0")) :url "https://github.com/RenWenshan/emacs-moz-controller")
