8a16361730fab342315bc6de5cfd99535e46309c
Sebastian Willert <willert@gmail.com>
2019-04-11 23:07:43 +0200

    Updated package sources

--- conf/01-packages.el
+++ conf/01-packages.el
@@ -1,8 +1,7 @@
 (require 'package)
 (setq package-archives
-      '(("ELPA" . "http://tromey.com/elpa/")
-        ("gnu" . "http://elpa.gnu.org/packages/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")
-        ("marmalade" . "http://marmalade-repo.org/packages/"))
+      '(("gnu" . "http://elpa.gnu.org/packages/")
+        ("melpa" . "http://melpa.org/packages/")
+        ("melpa-stable" . "http://stable.melpa.org/packages/"))
 )
 (package-initialize)
