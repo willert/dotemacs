05b29852572132fc8386b08b88223aa560f4693c
Sebastian Willert <willert@gmail.com>
2019-04-11 22:27:09 +0200

    Cosemtics

--- conf/70-x-windows.el
+++ conf/70-x-windows.el
@@ -3,14 +3,6 @@
 
   (require 'x-win)
 
-  (defun emacs-session-filename (session-id)
-    "Construct a filename to save the session in based on SESSION-ID
-     in  ~/.emacs.d/var"
-  (let ((basename (concat "session." session-id))
-        (emacs-dir user-emacs-directory))
-    (make-directory (expand-file-name (concat emacs-dir "var")))
-    (expand-file-name (concat emacs-dir "var/" basename))
-    ))
   (add-hook 'after-make-frame-functions
             (lambda (frame)
               (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 99)))
@@ -22,6 +14,16 @@
                  (start-process
                   "devilspie" "*devilspie*" "devilspie"
                   (expand-file-name "~/.emacs.d/devilspie.ds"))
-                 nil)
-                )))
-))
+                 nil))))
+
+
+  (defun emacs-session-filename (session-id)
+    "Construct a filename to save the session in based on SESSION-ID
+     in  ~/.emacs.d/var"
+    (let ((basename (concat "session." session-id))
+          (emacs-dir user-emacs-directory))
+      (make-directory (expand-file-name (concat emacs-dir "var")))
+      (expand-file-name (concat emacs-dir "var/" basename))
+      ))
+
+  ))
