e1d48e6a65c5429990da6eac735b70cd83a82c49
Sebastian Willert <willert@gmail.com>
2019-04-12 02:58:35 +0200

    Also set fringe background for magit

--- conf/41-magit.el
+++ conf/41-magit.el
@@ -1,6 +1,7 @@
 (defun sbw/magit-window-config ()
   "Used in `window-configuration-change-hook' to configure fringes for Magit."
-  (set-window-fringes nil 16 8))
+  (set-window-fringes nil 16 8)
+  (face-remap-add-relative 'fringe '(:background "#091916")))
 
 (defun sbw/magit-mode-hook ()
   "Custom `magit-mode' behaviours."
