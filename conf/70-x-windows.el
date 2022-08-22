443b7f4d3daaa661d6ff7595f3608c12a46e07ba
Sebastian Willert <willert@gmail.com>
2022-08-23 01:50:13 +0200

    Force face attributes and devilspie on every frame

--- conf/70-x-windows.el
+++ conf/70-x-windows.el
@@ -6,16 +6,23 @@
   (defun sbw/set-x-win-face-attributes (frame)
                   (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 99))
 
-  (add-hook 'after-make-frame-functions 'sbw/set-x-win-face-attributes)
-
-  (add-hook 'after-make-frame-functions
-            (lambda (frame)
+  (defun sbw/apply-devilspie-configuration (frame)
               (if (get-buffer "*devilspie*") nil
                 (set-process-query-on-exit-flag
                  (start-process
                   "devilspie" "*devilspie*" "devilspie"
                   (expand-file-name "~/.emacs.d/devilspie.ds"))
-                 nil))))
+                 nil)))
+
+  (add-hook 'after-make-frame-functions 'sbw/set-x-win-face-attributes)
+  (add-hook 'after-make-frame-functions 'sbw/apply-devilspie-configuration)
+
+  (defun sbw/reset-frame-configuration ()
+    "Set default x config for current frame"
+    (interactive)
+         (sbw/set-x-win-face-attributes (selected-frame))
+         (sbw/apply-devilspie-configuration (selected-frame))
+         )
 
 
   (defun emacs-session-filename (session-id)
