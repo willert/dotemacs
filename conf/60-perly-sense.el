57719a00877e35a9cbb7e965bd1a59a4810d5002
Sebastian Willert <willert@gmail.com>
2024-04-22 04:34:55 +0200

    Fixed readonly handling in ansi compilation buffers

--- conf/60-perly-sense.el
+++ conf/60-perly-sense.el
@@ -4,9 +4,9 @@
 
 (require 'ansi-color)
 (defun colorize-compilation-buffer ()
-  (toggle-read-only)
-  (ansi-color-apply-on-region (point-min) (point-max))
-  (toggle-read-only))
+  (let ((inhibit-read-only t))
+    (ansi-color-apply-on-region (point-min) (point-max)))
+  )
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
 
 ;; *** PerlySense Config ***
